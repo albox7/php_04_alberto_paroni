f3232eb60137b5f0f556780e4ae4a013be328971
Albox <albox7@gmail.com>
2026-02-21 21:21:53 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+
+
+$users = [
+	['name' => 'Luca', 'surname' => 'Bianchi', 'gender' => 'Uomo'],
+	['name' => 'Marta', 'surname' => 'Rossi', 'gender' => 'Donna'],
+	['name' => 'Roberto', 'surname' => 'Girelli', 'gender' => 'NB'],
+	['name' => 'Ludovica', 'surname' => 'Vento', 'gender' => 'Donna'],
+	['name' => 'Massimo', 'surname' => 'Lorenzi', 'gender' => 'NB'],
+	['name' => 'Federica', 'surname' => 'Anelli', 'gender' => 'Donna'],
+	['name' => 'Pietro', 'surname' => 'Caglio', 'gender' => 'Uomo']
+];
+
+
+
+foreach ($users as $user) {
+	
+	if($user['gender'] === 'Uomo') {
+		
+		echo "Buongiorno Sig. " . $user['name'] . " " . $user['surname'] . "<br>";
+
+	} elseif ($user['gender'] === 'Donna') {
+		
+		echo "Buongiorno Sig.ra " . $user['name'] . " " . $user['surname'] . "<br>";
+
+	} elseif ($user['gender'] === 'NB') {
+
+		echo "Buongiorno " . $user['name'] . " " . $user['surname'] . "<br>";
+	}
+}
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
